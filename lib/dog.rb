ee991df5259e6350920bc005e48ed9e4662461f1
Jackson Ramos <jackramos021495@gmail.com>
2020-05-11 23:22:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    save
+    self.save
   end
 
   def self.all
@@ -14,7 +14,7 @@ class Dog
   end
 
   def save
-    @@all << @name
+    @@all << self
   end
 
   def clear_all
