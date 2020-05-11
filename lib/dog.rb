c45110a86687ee632d781e7e3a7bae1650cd931a
Jackson Ramos <jackramos021495@gmail.com>
2020-05-11 23:16:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Add your code here
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @puppy = puppy
+    @@all << self
+  end
+
+  def self.all
+    @all
+  end
+  
+end
+
